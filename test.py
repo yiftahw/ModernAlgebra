893cb20594afa58d855a93fc51d95634e69cb53e
Uval Vered <uval@campus.technion.ac.il>
2020-11-04 21:19:23 +0200

    testing commit

---
+++ test.py
@@ -0,0 +1,10 @@
+def basicfunction(x)
+    new_number = x(x + x)
+    return new_number
+
+def main():
+    print("Hello World!")
+    print(basicfunction(5))
+
+if __name__ == "__main__":
+    main()
